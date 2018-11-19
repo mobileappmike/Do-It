f516167a1ef2cf3b0c315b6ef196243c40752ec9
Michael Miles <michael.gage.miles@gmail.com>
2018-11-19 14:35:33 -0800

    Initial Commit

---
+++ Do It/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Do It
+//
+//  Created by Michael Miles on 11/19/18.
+//  Copyright © 2018 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
