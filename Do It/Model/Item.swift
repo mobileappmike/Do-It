f88f62db2a16e55523548a5158f34052e27d203b
Michael Miles <michael.gage.miles@gmail.com>
2018-11-29 13:27:12 -0800

    added dateCreated property for sorting purposes

--- Do It/Model/Item.swift
+++ Do It/Model/Item.swift
@@ -12,6 +12,7 @@ import RealmSwift
 class Item: Object {
     @objc dynamic var title : String = ""
     @objc dynamic var done : Bool = false
+    @objc dynamic var dateCreated : Date?
     
     //in Realm, parent categories aren't assumed and inheritance must be set up manually
     var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
